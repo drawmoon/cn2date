4a07d0ce3497f98f0943b69a88651481dbbbae72
drawmoon <1340260725@qq.com>
2022-02-22 16:04:23 +0800

    解决在终端中执行示例报错 ImportError 的问题

--- examples/example_simple.py
+++ examples/example_simple.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+proj_dir = Path(__file__).parent.parent
+
+sys.path.append(str(proj_dir))
+
 from cn2date import parse
 
 words = ["2021-9-17", "2021年9月17日", "二零二一年九月十七日", "今天", "本季度", "2020年上半年"]
